e3bf3833837ee7d69c3cb0e3ad8996cc9fab9234
Antonio MalvaGomes <amalvag@g.clemson.edu>
2011-12-22 17:09:01 -0500

    Functionally decomposed the main script.

--- Resources/js/main.js
+++ Resources/js/main.js
@@ -1,30 +1,48 @@
 // Create Global Variables
-// var Ti = {fs: Titanium.Filesystem};
+var Ti = {
+	fs : Titanium.Filesystem
+};
 
 // Configure the layout here
-	$('body').layout({
-		applyDefaultStyles : true
-	});
-				
-//Set the window height
+$('body').layout({
+	applyDefaultStyles : true
+});
+
+function resetWindowPosition()
+{
 	var currentWindow = Titanium.UI.getMainWindow();
 	currentWindow.height = 600;
 	currentWindow.width = 800;
 	currentWindow.x = Math.round((screen.width / 2) - 300);
 	currentWindow.y = Math.round((screen.height / 2) - 200);
+}
 
-// Populate menubar
+function loadMenuBar()
+{
 	var new_menu = Titanium.UI.createMenu();
 	var file_submenu = new_menu.addItem("File");
 	var about_submenu = new_menu.addItem("About");
-	file_submenu.addItem("Exit", function(){
+	file_submenu.addItem("Exit", function() {
 		Titanium.App.exit();
 	})
 	Titanium.UI.setMenu(new_menu);
+}
 
-// Populate data
+function loadTitleBar(titleString)
+{
 	var tSource = $('#north-template').html();
 	var temp = Handlebars.compile(tSource);
-	var context = {north: "Template"};
+	var context = {
+		north : titleString
+	};
 	$('#north').html(temp(context).toString());
+}
 
+function initialize()
+{
+	resetWindowPosition();
+	loadMenuBar();
+	loadTitleBar("Template");
+}
+
+initialize();
