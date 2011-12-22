11d9a66add27c0278fe27fa528f385e0dd7fe1b7
unknown <josh@InternDesktop.HillSouth.local>
2011-12-22 14:58:39 -0500

    Initial commit of ticelerator

---
+++ Resources/js/main.js
@@ -0,0 +1,33 @@
+//Create Global Variables
+var Ti = {fs: Titanium.Filesystem};
+
+//Configure the layout here
+$('body').layout({
+	applyDefaultStyles : true
+});
+				
+//Set the window height
+var currentWindow = Titanium.UI.getMainWindow();
+currentWindow.height = 600;
+currentWindow.width = 800;
+currentWindow.x = Math.round((screen.width / 2) - 300);
+currentWindow.y = Math.round((screen.height / 2) - 200);
+
+//Populate menubar
+var new_menu = Titanium.UI.createMenu();
+var file_submenu = new_menu.addItem("File");
+var about_submenu = new_menu.addItem("About");
+file_submenu.addItem("Exit", function(){
+	Titanium.App.exit();
+})
+Titanium.UI.setMenu(new_menu);
+
+//Populate data
+var view = Ember.View.create({
+  templateName: 'title-panel',
+  title: "Template"
+});
+view.appendTo('#titlePanel');
+
+//Create the namespace
+window.App = Ember.Application.create();
